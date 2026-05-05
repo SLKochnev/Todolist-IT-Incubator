be8e5066d90e41f35826cbdf13c7b378d721fdfe
Slava Kochnev <sl.kochnev@mail.ru>
2026-05-05 17:37:01 +0500

    Добавил генерацию id с помощью uuid, поработал с useRef, добавил добавление таски через 'Enter'

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 import './App.css'
 import {TodolistItem} from './TodolistItem'
+import {v1} from 'uuid'
 
 export type Task = {
-  id: number
+  id: string
   title: string
   isDone: boolean
 }
@@ -13,12 +14,12 @@ export type FilterValue = 'All' | 'Active' | 'Completed'
 export const App = () => {
 
   let [tasks, setTasks] = useState<Task[]> ([
-    { id: 1, title: 'HTML&CSS', isDone: true },
-    { id: 2, title: 'JS', isDone: true },
-    { id: 3, title: 'ReactJS', isDone: false },
-    { id: 4, title: 'Redux', isDone: false },
-    { id: 5, title: 'Typescript', isDone: false },
-    { id: 6, title: 'RTK query', isDone: false },
+    { id: v1(), title: 'HTML&CSS', isDone: true },
+    { id: v1(), title: 'JS', isDone: true },
+    { id: v1(), title: 'ReactJS', isDone: false },
+    { id: v1(), title: 'Redux', isDone: false },
+    { id: v1(), title: 'Typescript', isDone: false },
+    { id: v1(), title: 'RTK query', isDone: false },
   ])
 
   const [filterValue, setFilterValue] = useState<FilterValue>('All')
@@ -36,17 +37,30 @@ export const App = () => {
     }
   
 
- const deleteTask = (taskId: number) => {
+ const deleteTask = (taskId: string) => {
   const filteredTasks = tasks.filter(task => {
     return task.id !== taskId
   })
     setTasks(filteredTasks)
  }
- 
+
+ const addTask = (title: string) => {
+  const newTask: Task = {
+    id: v1(),
+    title,
+    isDone: false
+  }
+  setTasks([newTask, ...tasks])
+ }
 
   return (
       <div className="app">
-        <TodolistItem title="What to learn" tasks={filteredTasks} deleteTask={deleteTask} filterTasks={filterTask} />
+        <TodolistItem 
+          title="What to learn" 
+          tasks={filteredTasks} 
+          deleteTask={deleteTask} 
+          filterTasks={filterTask}
+          addTask={addTask} />
       </div>
   )
 }
--- src/TodolistItem.tsx
+++ src/TodolistItem.tsx
@@ -1,30 +1,50 @@
 import type {Task} from './App'
 import {Button} from './Button'
 import { FilterValue } from './App'
+import { useState, ChangeEvent, KeyboardEvent } from 'react'
 
 type Props = {
   title: string
   tasks: Task[]
-  deleteTask: (taskId: number) => void
+  deleteTask: (taskId: string) => void
   filterTasks: (value: FilterValue) => void
+  addTask: (title: string) => void
 }
 
-export const TodolistItem = ({title, tasks, deleteTask, filterTasks}: Props) => {
+export const TodolistItem = ({title, tasks, deleteTask, filterTasks, addTask}: Props) => {
+  
+  const [taskTitle, setTasksTitle] = useState('')
+
+  const createTaskHandler = () => {
+    addTask(taskTitle)
+    setTasksTitle('')
+  }
+
+  const changeTaskTitleHandler = (event: ChangeEvent<HTMLInputElement>) => setTasksTitle(event.currentTarget.value);
+  const createTaskOnEnterHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+              createTaskHandler()
+            }
+          }
+
   return (
       <div>
         <h3>{title}</h3>
         <div>
-          <input/>
-          <Button title={'+'} />
+          <input value={taskTitle} onChange={ changeTaskTitleHandler } onKeyDown={ createTaskOnEnterHandler }/>
+          <Button title={'+'} onClick={createTaskHandler} />
         </div>
         {tasks.length === 0 ? (
             <p>Тасок нет</p>
         ) : (
             <ul>
               {tasks.map(task => {
+                  const deleteTaskHandler = () => {
+                    deleteTask(task.id)
+                  }
                 return (
                     <li key={task.id}>
-                      <Button title={'x'} onClick={() => deleteTask(task.id)} />
+                      <Button title={'x'} onClick={ deleteTaskHandler } />
                       <input type="checkbox" checked={task.isDone} />
                       <span>{task.title}</span>
                     </li>
